1cadc21debc3b6de6e7f4fdb6479e0a2870de5d6
Matt Ferlaino <35575372+Fernakus@users.noreply.github.com>
2020-03-20 15:36:05 -0400

    added gedit install to master-installer and updated set-permissions to point to $PWD

--- Kali-Linux/Installer-Scripts/Master-Installer.sh
+++ Kali-Linux/Installer-Scripts/Master-Installer.sh
@@ -25,8 +25,8 @@ fi
 echo "-----------------------------"
 echo "Setting permissions/ownership"
 echo "-----------------------------"
-chown root -R /root/Desktop/Kali-Linux
-chmod 777 -R /root/Desktop/Kali-Linux
+chown root -R $PWD
+chmod 777 -R $PWD
 
 
 # Download Updates
@@ -46,6 +46,9 @@ echo "------------"
 echo "Autoremoving"
 echo "-------------"
 apt-get -y autoremove
+
+# Download gedit
+apt-get install -y gedit
 
 # Download/Install Tor Software
 echo "---------------"
@@ -237,7 +240,7 @@ echo "Installing Komodo"
 echo "-----------------"
 sudo add-apt-repository ppa:mystic-mirage/komodo-edit
 sudo apt-get update
-sudo apt-get install komodo-edit
+sudo apt-get install -y komodo-edit
 
 # Install Chromium
 echo "---------------------------"
@@ -263,11 +266,11 @@ echo "Installing Sublime"
 echo "------------------"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get install sublime-text
+sudo apt-get install -y sublime-text
 
 # Create Desktop Links
 cd
-cd Desktop 
+cd Desktop
 echo "[Desktop Entry]
 Name=Start x11vnc
 Comment=Start x11vnc
@@ -284,7 +287,7 @@ Exec=chromium --no-sandbox
 Icon=applications-internet
 Type=Application " > Chromium.desktop
 
-# Copy Identity 
+# Copy Identity
 cp -f /root/Desktop/Fern-Pen-Testing/Identity > /root/Desktop
 
 # Downloading Final Set Of Updates
@@ -350,4 +353,4 @@ echo "--------------------"
 echo "----------------------------"
 echo "Fernwood.Penetration.Testing"
 echo "----------------------------"
-reboot 
+reboot
